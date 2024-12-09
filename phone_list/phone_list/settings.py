405b4d88bea5cacdb7812dc7863a57a117bbf597
Anel Ablaeva <ablaevanel@gmail.com>
2024-12-10 02:50:25 +0300

    г

--- phone_list/phone_list/settings.py
+++ phone_list/phone_list/settings.py
@@ -11,26 +11,25 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
 import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(os.path.join(BASE_DIR/".eVar", ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-vdhb7jo)8xkzq_yyxk7hw170+(#g0%+(#80qq(j2%-=o-m!=@j'
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG")
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    "127.0.0.1",
     "localhost",
-    "practice-5-7.onrender.com"
+    "127.0.0.1",
+    "practice-5-8.onrender.com"
 ]
 
 
