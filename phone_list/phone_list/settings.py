6768815ac05a4dfb8123216f680fc023c1fcd4d1
Anel Ablaeva <ablaevanel@gmail.com>
2024-12-10 00:48:52 +0300

    updates

--- phone_list/phone_list/settings.py
+++ phone_list/phone_list/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vdhb7jo)8xkzq_yyxk7hw170+(#g0%+(#80qq(j2%-=o-m!=@j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
